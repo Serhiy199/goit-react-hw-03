0c299f168455503fb8f3b797d77ac24a0d1abded
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-02-26 00:21:52 +0200

    initial-code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+function App() {
+    return <></>;
+}
+
+export default App;
